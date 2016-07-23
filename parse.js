7300263bcbaebd3ea82d5ab00c4fd8c1d538e6d6
Adrien Joly <adrien@whyd.com>
2016-07-23 15:56:13 +0200

    json model/format update: part->scene, actor->character

--- parse.js
+++ parse.js
@@ -10,13 +10,13 @@ function parseText(text) {
     .split('\n[1]')[0] // delete references at end of file
     .split(/SC.NE /g);
 
-  // part 0 = actors & context
-  var actors = [];
+  // part 0 = characters & context
+  var characters = [];
   var context = [];
   var [ text, act ] = parts.shift().split('ACTE ');
   var intro = text.split('ACTEURS :')[1] || text.split(/Acte [^\n]*\n\n/)[1] || text;
   intro.split('\n').forEach(l => {
-    (ACTOR_PRESENTATION.test(l) ? actors : context).push(l);
+    (ACTOR_PRESENTATION.test(l) ? characters : context).push(l);
   });
 
   // following parts
@@ -24,16 +24,16 @@ function parseText(text) {
 
   // parse dialogue
   parts = parts.map((part, index) => {
-    var all = part.split(ACTOR_SPEECH); // alternance of actor and actor's speech
-    var actorList = all.shift();
-    var actors = all.filter((a, i) => (i + 1) % 2);
+    var all = part.split(ACTOR_SPEECH); // alternance of character and character's speech
+    var characterList = all.shift();
+    var characters = all.filter((a, i) => (i + 1) % 2);
     var speech = all.filter((a, i) => i % 2);
     return {
       act: act.split(',')[0],
-      part: index + 1,
-      dialogue: actors.map((a, i) => {
+      scene: index + 1,
+      dialogue: characters.map((a, i) => {
         return {
-          actor: a,
+          character: a,
           text: speech[i]
         };
       })
@@ -41,9 +41,9 @@ function parseText(text) {
   });
 
   return {
-    actors: actors,
+    characters: characters,
     context: context,
-    parts: parts
+    scenes: parts
   };
 }
 
