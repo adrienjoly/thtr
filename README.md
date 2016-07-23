10a53d67a449436e3ff966e56eb1ca562cd773b6
Adrien Joly <adrien@whyd.com>
2016-07-23 18:04:01 +0200

    add GET /api/plays endpoint => list of plays

--- README.md
+++ README.md
@@ -15,5 +15,6 @@ npm test # (to be run in another terminal)
 # api endpoints
 
 - `GET /` says hello
-- `GET /api/play/:play/act/:act` returns a play's act in JSON format, e.g. [fourberies-acte1](https://github.com/adrienjoly/thtr/blob/master/plays/fourberies/acte1.json)
+- `GET /api/plays` returns the list of plays
 - `GET /api/play/:play` returns a play's metadata in JSON format (`title` and `characters`, as read from the act 1 of that play)
+- `GET /api/play/:play/act/:act` returns a play's act in JSON format, e.g. [fourberies-acte1](https://github.com/adrienjoly/thtr/blob/master/plays/fourberies/acte1.json)
--- server.js
+++ server.js
@@ -2,14 +2,15 @@ var fs = require('fs')
 var express = require('express')
 var app = express()
 
-var PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
+const PATH_PLAYS = './plays/'
 
-const makeActFileName = ({ play, act }) => './plays/' + play + '/acte' + act + '.json'
+const makeActFileName = ({ play, act }) => PATH_PLAYS + play + '/acte' + act + '.json'
 const readActFile = param => fs.readFileSync(makeActFileName(param)).toString()
 const getActJson = param => JSON.parse(readActFile(param))
 const getPlayJson = play => {
-  var { title, characters } = JSON.parse(readActFile({ play: play, act: 1 }));
-  return { title, characters };
+  var { title, characters } = JSON.parse(readActFile({ play: play, act: 1 }))
+  return { title, characters }
 }
 
 app.get('/', (req, res) =>
@@ -18,11 +19,17 @@ app.get('/', (req, res) =>
 app.get('/api/file/:file', (req, res) =>
   res.json(JSON.parse(fs.readFileSync(req.params.file).toString())))
  
-app.get('/api/play/:play/act/:act', (req, res) => res.json(getActJson(req.params)))
- 
+app.get('/api/plays', (req, res) => {
+  fs.readdir(PATH_PLAYS, (err, plays) =>
+    res.json(plays.filter(id => id !== '.DS_Store').map(id => // excludes .DS_Store from list of plays
+      Object.assign({ id: id }, getPlayJson(id))
+    ))
+  )
+})
+
 app.get('/api/play/:play', (req, res) => res.json(getPlayJson(req.params.play)))
 
-// TODO: list of plays
-
+app.get('/api/play/:play/act/:act', (req, res) => res.json(getActJson(req.params)))
+ 
 app.listen(PORT)
 console.log('server running on port', PORT)
