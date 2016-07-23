3359458e07dec3dccd7f2a5f9256f12ab627e8ff
Adrien Joly <adrien@whyd.com>
2016-07-23 13:32:06 +0200

    refacto on working parsing! => exported json output to file

--- parse.js
+++ parse.js
@@ -7,39 +7,47 @@ var readFromFile = function (filepath) {
   return require('fs').readFileSync(filepath).toString();
 };
 
-//var text = readFromFile('lbg-acte1.txt');
-var text = readFromFile('fourberies-acte1.txt');
+function parseText(text) {
+  var parts = text
+    .split('\n[1]')[0] // delete references at end of file
+    .split(/SC.NE /g);
 
-text = text.split('\n[1]')[0]; // delete references at end of file
+  // part 0 = actors & context
+  var intro = parts.shift().split('ACTEURS :')[1].split('ACTE ')[0];
+  var actors = intro.split('\n').filter(l => ACTOR_PRESENTATION.test(l));
+  var context = intro.split('\n').filter(l => !ACTOR_PRESENTATION.test(l));
 
-var parts = text.split(/SC.NE /g);
+  // following parts
+  parts = parts.map(p => p.split('\n').slice(1).join('\n')); // ignore part number from each part's text
 
-// part 0 = actors & context
-var intro = parts.shift().split('ACTEURS :')[1].split('ACTE ')[0];
-var actors = intro.split('\n').filter(l => ACTOR_PRESENTATION.test(l));
-var context = intro.split('\n').filter(l => !ACTOR_PRESENTATION.test(l));
-console.log('actors:', actors);
-console.log('context:', context);
+  // parse dialogue
+  parts = parts.map((part, index) => {
+    var all = part.split(ACTOR_SPEECH); // alternance of actor and actor's speech
+    var actorList = all.shift();
+    var actors = all.filter((a, i) => (i + 1) % 2);
+    var speech = all.filter((a, i) => i % 2);
+    return {
+      act: 1,
+      part: index + 1,
+      dialogue: actors.map((a, i) => {
+        return {
+          actor: a,
+          text: speech[i]
+        };
+      })
+    };
+  });
 
-// following parts
-parts = parts.map(p => p.split('\n').slice(1).join('\n')); // ignore part number from each part's text
-
-// parse dialogue
-parts = parts.map((part, index) => {
-  var all = part.split(ACTOR_SPEECH); // alternance of actor and actor's speech
-  var actorList = all.shift();
-  var actors = all.filter((a, i) => (i + 1) % 2);
-  var speech = all.filter((a, i) => i % 2);
   return {
-    act: 1,
-    part: index + 1,
-    dialogue: actors.map((a, i) => {
-      return {
-        actor: a,
-        text: speech[i]
-      };
-    })
+    actors: actors,
+    context: context,
+    parts: parts
   };
-});
+}
 
-console.log('parts:', JSON.stringify(parts, null, '  '));
+
+
+//var text = readFromFile('lbg-acte1.txt');
+var parsed = parseText(readFromFile('fourberies-acte1.txt'));
+console.log(JSON.stringify(parsed, null, '  '));
+
