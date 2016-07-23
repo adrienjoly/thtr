5f5394e26ddf8785d2595640f4791786323e38b4
Adrien Joly <adrien@whyd.com>
2016-07-23 18:43:46 +0200

    trim spaces from emoji names

--- fetch-emoji.js
+++ fetch-emoji.js
@@ -2,9 +2,12 @@ const request = require('request')
 
 const EMOJI_DEF = /data-c="([^"]+)"\>([^\<]+)/g;
 
+const processName = name => name
+  .trim()
+
 request('http://facebook-emoticons.fr.downloadastro.com/tools/', (error, response, body) => {
   let m
   while (m = EMOJI_DEF.exec(body)) {
-    console.log(m.slice(1, 3).join(', '))
+    console.log([ m[1], processName(m[2]) ].join(', '))
   }
 })
