cd8e61acb3584d9a465e39e7b92fc2cc8757c70c
Adrien Joly <adrien@whyd.com>
2016-07-23 13:15:58 +0200

    cleaning

--- parse.js
+++ parse.js
@@ -1,6 +1,7 @@
 // text parser from http://www.toutmoliere.net/acte-1,405363.html
 
 const ACTOR_PRESENTATION = /^[^a-z0-9\.]{3,}.+\.$/;
+const ACTOR_SPEECH = /\n([^a-z0-9\.]{3,}[^\.]+)\.\- /;
 
 var readFromFile = function (filepath) {
   return require('fs').readFileSync(filepath).toString();
@@ -13,20 +14,19 @@ text = text.split('\n[1]')[0]; // delete references at end of file
 
 var parts = text.split(/SC.NE /g);
 
-var intro = parts.shift();
-intro = intro.split('ACTEURS :')[1];
-intro = intro.split('ACTE ')[0];
-//let [ actors, context ] = intro.split('\n\n');
-//var actors = intro.split('/n').filter(l => /^[^a-z0-9\.]{3,}[^\.]+\.$/.test(l)).join('\n');
+// part 0 = actors & context
+var intro = parts.shift().split('ACTEURS :')[1].split('ACTE ')[0];
 var actors = intro.split('\n').filter(l => ACTOR_PRESENTATION.test(l));
 var context = intro.split('\n').filter(l => !ACTOR_PRESENTATION.test(l));
 console.log('actors:', actors);
 console.log('context:', context);
 
-parts = parts.map((p) => p.split('\n').slice(1).join('\n'));
-/*
-console.log('parts:', parts.map((p) => p
-  .split(/\n([^a-z0-9\.]{3,}[^\.]+)\.\- /)
+// following parts
+parts = parts.map(p => p.split('\n').slice(1).join('\n')); // ignore part number from each part's text
+
+parts = parts.map(p => p
+  .split(ACTOR_SPEECH)
   .filter((a, i) => i % 2)
-));
-*/
+);
+
+console.log('parts:', parts);
