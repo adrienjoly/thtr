ff8643d2fcfab3c65b6a44ce5996d832ab57c1e3
Adrien Joly <adrien@whyd.com>
2016-07-24 15:06:46 +0200

    simplify gen-play-metadata

--- gen-play-metadata.js
+++ gen-play-metadata.js
@@ -4,12 +4,10 @@ const PATH_PLAYS = './plays/'
 
 const readPlay = id => JSON.parse(fs.readFileSync(PATH_PLAYS + id + '/acte1.json').toString())
 
-const getMetadata = json => {
-  var { title, characters } = json
-  return { title, characters }
+const genPlayMetadata = id => {
+  var { title, characters } = readPlay(id)
+  return { id: id, title, characters }
 }
-
-const genPlayMetadata = id => Object.assign({ id: id }, getMetadata(readPlay(id)))
 
 fs.readdir(PATH_PLAYS, (err, plays) =>
   plays.filter(id => id.indexOf('.') == -1).forEach(id => {
