31c5c372152d3441292826aff98761cb5813608d
Adrien Joly <adrien@whyd.com>
2016-07-24 17:32:28 +0200

    add assets for le bourgeois gentilhomme

--- gen-play-metadata.js
+++ gen-play-metadata.js
@@ -4,18 +4,26 @@ const PATH_PLAYS = './plays/'
 const URL_PLAYS = '/play/'
 
 const removeAccents = str => !str ? '' : str
+  .replace(/[’`"]/g, "'")
+  .replace(/[ç]/gi, 'c')
   .replace(/[àâä]/gi, "a")
   .replace(/[éèêë]/gi, "e")
   .replace(/[îï]/gi, "i")
   .replace(/[ôö]/gi, "o")
   .replace(/[ùûü]/gi, "u")
 
+const addSeparators = str => str.replace(/[^a-z0-9']+/g, '-')
+
+const removeTrailingShit = str => str.replace(/-+$/, '')
+
+const toFileName = str => removeTrailingShit(addSeparators(removeAccents(str.toLowerCase())))
+
 const readPlay = id => JSON.parse(fs.readFileSync(PATH_PLAYS + id + '/acte1.json').toString())
 
 const genPlayMetadata = id => {
   var { title, characters } = readPlay(id)
   const appendAvatar = char => {
-    const avatarFile = id + '/avatars/' + removeAccents(char.name.toLowerCase()) + '.png'
+    const avatarFile = id + '/avatars/' + toFileName(char.name) + '.png'
     return !fs.existsSync(PATH_PLAYS + avatarFile) ? char : Object.assign(char, {
       avatarFile: URL_PLAYS + avatarFile
     })
