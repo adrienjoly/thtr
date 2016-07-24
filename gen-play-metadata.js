c118c31c6b0459e630a355ef5f058a9922038542
Adrien Joly <adrien@whyd.com>
2016-07-24 15:23:49 +0200

    add avatarFile attribute in plays' metadata + GET access to avatar files

--- gen-play-metadata.js
+++ gen-play-metadata.js
@@ -1,12 +1,23 @@
 const fs = require('fs')
 
 const PATH_PLAYS = './plays/'
+const URL_PLAYS = '/play/'
 
 const readPlay = id => JSON.parse(fs.readFileSync(PATH_PLAYS + id + '/acte1.json').toString())
 
 const genPlayMetadata = id => {
   var { title, characters } = readPlay(id)
-  return { id: id, title, characters }
+  const appendAvatar = char => {
+    const avatarFile = id + '/avatars/' + char.name.toLowerCase() + '.png'
+    return !fs.existsSync(PATH_PLAYS + avatarFile) ? char : Object.assign(char, {
+      avatarFile: URL_PLAYS + avatarFile
+    })
+  }
+  return {
+    id: id,
+    title,
+    characters: characters.map(appendAvatar)
+  }
 }
 
 fs.readdir(PATH_PLAYS, (err, plays) =>
--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const getPlayJson = play => JSON.parse(fs.readFileSync(PATH_PLAYS + play + '.jso
 app.get('/', (req, res) =>
   res.send('Hey, how are you today?'))
  
+app.get('/play/:play/avatars/:char', (req, res) =>
+  fs.readFile(req.path.replace('/play/', PATH_PLAYS), (err, data) => res.send(data)))
+ 
 app.get('/api/file/:file', (req, res) =>
   res.json(JSON.parse(fs.readFileSync(req.params.file).toString())))
  
