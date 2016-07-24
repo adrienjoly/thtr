cb5f9cf22f2aea7992b629a845626edbdc9753dc
Adrien Joly <adrien@whyd.com>
2016-07-24 15:04:49 +0200

    gen-play-metadata is now driven by node.js

--- gen-play-metadata.js
+++ gen-play-metadata.js
@@ -1,10 +1,18 @@
-const readJsonFile = file => JSON.parse(require('fs').readFileSync(file).toString())
+const fs = require('fs')
+
+const PATH_PLAYS = './plays/'
+
+const readPlay = id => JSON.parse(fs.readFileSync(PATH_PLAYS + id + '/acte1.json').toString())
 
 const getMetadata = json => {
   var { title, characters } = json
   return { title, characters }
 }
 
-const file = process.argv[2] + '/acte1.json'
+const genPlayMetadata = id => Object.assign({ id: id }, getMetadata(readPlay(id)))
 
-console.log(JSON.stringify(getMetadata(readJsonFile(file)), null, '  '))
+fs.readdir(PATH_PLAYS, (err, plays) =>
+  plays.filter(id => id.indexOf('.') == -1).forEach(id => {
+    fs.writeFileSync(PATH_PLAYS + id + '.json', JSON.stringify(genPlayMetadata(id), null, '  ') + '\n')
+  })
+)
